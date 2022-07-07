047c89eb2bd37039345d6ad4708f80f130cc42d0
Sumin <sumin.kim.dev@gmail.com>
2022-07-07 09:48:22 +0900

    Design: Modal 디자인 구현

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,20 +1,53 @@
 import Header from '../../shared/Header';
 import styles from "./Main.module.css";
 import imagA from '../../docs/layout1.jpg';
+import { useState } from 'react';
+import Modal from '../utils/Modal';
+import { Link } from 'react-router-dom';
 
 function Main() {
+    const [open, setOpen] = useState(false);
+
+    function openModal() {
+        setOpen(true);
+    }
+    function closeModal() {
+        setOpen(false);
+    }
+
+    // function handleAddItem() {
+    //     return ['wow'];
+    // }
+
+
     return (
         <div>
             <Header />
             <div className={styles.container}>
                 <div className={styles.addBook}>
-                    <button>추가</button>
+                    <button onClick={openModal}>추가</button>
                 </div>
-                <div className={styles.bookInfo}>
-                    <img src={imagA} alt="책 표지" />
-                    <div className={styles.title}>책 제목</div>
-                    <div className={styles.graph}>달성 그래프</div>
-                </div>
+                <Link to="./detail">
+                    <div className={styles.bookInfo}>
+                        <img src={imagA} alt="책 표지" />
+                        <div className={styles.title}>책 제목</div>
+                        <div className={styles.graph}>달성 그래프</div>
+                    </div>
+                </Link>
+                <Link to="./detail">
+                    <div className={styles.bookInfo}>
+                        <img src={imagA} alt="책 표지" />
+                        <div className={styles.title}>책 제목</div>
+                        <div className={styles.graph}>달성 그래프</div>
+                    </div>
+                </Link>
+                <Link to="./detail">
+                    <div className={styles.bookInfo}>
+                        <img src={imagA} alt="책 표지" />
+                        <div className={styles.title}>책 제목</div>
+                        <div className={styles.graph}>달성 그래프</div>
+                    </div>
+                </Link>
                 <div className={styles.bookInfo}>
                     <img src={imagA} alt="책 표지" />
                     <div className={styles.title}>책 제목</div>
@@ -36,6 +69,7 @@ function Main() {
                     <div className={styles.graph}>달성 그래프</div>
                 </div>
             </div>
+            <Modal open={open} close={closeModal} />
         </div>
     )
 }
