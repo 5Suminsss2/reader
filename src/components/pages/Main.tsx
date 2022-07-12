8def8eb8a5ad7ce03ac0b44e64abfaadb154fdc9
Sumin <sumin.kim.dev@gmail.com>
2022-07-12 17:30:53 +0900

    Design: Modal 디자인 수정

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -6,6 +6,28 @@ import Modal from '../utils/Modal';
 import { Link } from 'react-router-dom';
 
 function Main() {
+    const initData = [
+        {
+            author: "소주",
+            content: "저는 소주를 안 좋아해요!",
+            image:
+                "https://cdn.pixabay.com/photo/2017/10/22/08/02/bottle-2877005__340.jpg",
+        },
+        {
+            name: "맥주",
+            content: "요즘 흑맥주가 좋아요.",
+            image:
+                "https://cdn.pixabay.com/photo/2015/06/24/13/31/beer-820011_1280.jpg",
+        },
+        {
+            name: "샴페인",
+            content: "샴페인 좋아요~ 샴페인 좋아요~",
+            image:
+                "https://cdn.pixabay.com/photo/2017/03/27/13/36/champagne-2178775__340.jpg",
+        },
+    ];
+
+
     const [open, setOpen] = useState(false);
 
     function openModal() {
@@ -15,9 +37,12 @@ function Main() {
         setOpen(false);
     }
 
-    // function handleAddItem() {
-    //     return ['wow'];
-    // }
+    const [data, setData] = useState(initData);
+
+    let handleAddItem = (item: any) => {
+        setData([...data, item]);
+    };
+
 
 
     return (
@@ -29,7 +54,7 @@ function Main() {
                 </div>
 
                 <Link to="./detail" className={styles.bookInfo}>
-                    <img src={imagA} alt="책 표지" />
+                    <img src="https://bookthumb-phinf.pstatic.net/cover/225/236/22523637.jpg?type=m140&udate=20220617" alt="책 표지" />
                     <div className={styles.title}>책 제목</div>
                     <div className={styles.graph}>달성 그래프</div>
                 </Link>
@@ -54,7 +79,7 @@ function Main() {
                     <div className={styles.graph}>달성 그래프</div>
                 </Link>
             </div>
-            <Modal open={open} close={closeModal} />
+            <Modal open={open} close={closeModal} handleAddItem={handleAddItem} />
         </div >
     )
 }
--- src/components/utils/Modal.tsx
+++ src/components/utils/Modal.tsx
@@ -4,31 +4,25 @@ import { useState } from 'react';
 type ModalProps = {
     open: boolean;
     close: any;
+    handleAddItem: any;
 };
 
-function Modal({ open, close }: ModalProps): JSX.Element {
-
-    const [name, setName] = useState("");
-    const [content, setContent] = useState("");
+function Modal({ open, close, handleAddItem }: ModalProps): JSX.Element {
     const [image, setImage] = useState("");
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [publisher, setPublisher] = useState("");
+    const [date, setDate] = useState("");
+    const [page, setPage] = useState("");
 
     const onSubmit = (e: any) => {
         e.preventDefault();
-        console.log(name, content, image);
+        console.log(image, title, author, publisher, date, page);
+        handleAddItem({ image, title, author, publisher, date, page });
         close();
     };
 
-    function setThumbnail(event: any) {
-        const reader = new FileReader();
 
-        reader.onload = function (event: any) {
-            const img = document.createElement("img");
-            img.setAttribute("src", event.target.result);
-            (document.querySelector("#bookImg") as HTMLInputElement).appendChild(img);
-        };
-
-        reader.readAsDataURL(event.target.files[0]);
-    }
 
     return (
         <>
@@ -37,39 +31,33 @@ function Modal({ open, close }: ModalProps): JSX.Element {
                     <form className={styles.modal} onSubmit={onSubmit}>
                         <span className={styles.modalClose} onClick={close}>X</span>
                         <div className={styles.container}>
-                            <div className={styles.bookImage}>
-                                {/*https://sinna94.tistory.com/entry/JavaScript-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C-%EB%AF%B8%EB%A6%AC%EB%B3%B4%EA%B8%B0-%EB%A7%8C%EB%93%A4%EA%B8%B0*/}
-                                <label htmlFor="image" className={styles.label}>이미지</label>
-                                <input id="image" type="file" className={styles.imageInput} placeholder="URL" value={image}
-                                    onChange={(e) => {
-                                        setImage(e.target.value);
-                                        setThumbnail(e);
-                                    }} />
-                                <div id="bookImg" className={styles.bookcover}></div>
-
-                                <button className={styles.saveButton}>도전하기</button>
-                            </div>
-
                             <div className={styles.bookInfo}>
                                 <label htmlFor="title" className={styles.label}>제목</label>
-                                <input id="title" type="text" className={styles.modalInput} placeholder="천 개의 파랑" value={name}
-                                    onChange={(e) => setName(e.target.value)} />
+                                <input id="title" type="text" className={styles.modalInput} placeholder="천 개의 파랑" value={title}
+                                    onChange={(e) => setTitle(e.target.value)} />
 
                                 <label htmlFor="author" className={styles.label}>저자</label>
-                                <input id="author" type="text" className={styles.modalInput} placeholder="천선란" value={name}
-                                    onChange={(e) => setName(e.target.value)} />
+                                <input id="author" type="text" className={styles.modalInput} placeholder="천선란" value={author}
+                                    onChange={(e) => setAuthor(e.target.value)} />
 
                                 <label htmlFor="publisher" className={styles.label}>출판사</label>
-                                <input id="publisher" type="text" className={styles.modalInput} placeholder="허블" value={name}
-                                    onChange={(e) => setName(e.target.value)} />
+                                <input id="publisher" type="text" className={styles.modalInput} placeholder="허블" value={publisher}
+                                    onChange={(e) => setPublisher(e.target.value)} />
 
                                 <label htmlFor="date" className={styles.label}>목표 날짜</label>
-                                <input id="date" type="text" className={styles.modalInput} placeholder="2023-01-01" value={content}
-                                    onChange={(e) => setContent(e.target.value)} />
+                                <input id="date" type="text" className={styles.modalInput} placeholder="2023-01-01" value={date}
+                                    onChange={(e) => setDate(e.target.value)} />
 
                                 <label htmlFor="page" className={styles.label}>총 페이지</label>
-                                <input id="page" type="text" className={styles.modalInput} placeholder="376" value={content}
-                                    onChange={(e) => setContent(e.target.value)} />
+                                <input id="page" type="number" className={styles.modalInput} placeholder="376" value={page}
+                                    onChange={(e) => setPage(e.target.value)} />
+                                <label htmlFor="image" className={styles.label}>이미지</label>
+                                <input id="image" type="text" className={styles.modalInput} placeholder="URL" value={image}
+                                    onChange={(e) => {
+                                        setImage(e.target.value);
+
+                                    }} />
+                                <button className={styles.saveButton}>도전하기</button>
                             </div>
                         </div>
                     </form>
